426d4ae2fb1b9c0b7ad52e6c8561baf9d8193fae
Parth <1008parth@gmail.com>
2026-01-15 17:04:05 +0530

    Updates findCommentSymbols

--- cloc/utils.py
+++ cloc/utils.py
@@ -2,6 +2,7 @@
 from types import MappingProxyType
 import csv
 import os
+from typing import Mapping, Optional, Union
 
 from cloc.config import LANGUAGES
 
@@ -24,32 +25,39 @@ def getVersion():
         else:
             print(f"py-cloc {version}")
 
-def findCommentSymbols(extension: str, symbolMapping: dict[str, dict[str, str]] | None = None) -> bytes | tuple[bytes, bytes] | tuple[bytes, tuple[bytes, bytes]]:
+def findCommentSymbols(extension: str,
+                       symbolMapping: Union[Mapping[str, Mapping[str, str]], None] = None
+                       ) -> Union[bytes, tuple[bytes, bytes], tuple[bytes, tuple[bytes, bytes]]]:
         '''### Find symbols that denote a comment for a specific language
         
         #### args
         extension: File extension of the language\n
         symbolMapping: Mapping of file extensions and their corresponding symbols. Keys are `symbols` for single-line comments and `multilined` for multi-line comments. See `languages.json` for the actual mapping'''
 
-        extension = extension.lower()
+        extension = extension.strip().lower()
         if not symbolMapping:
             symbolMapping = LANGUAGES
 
-        singleLineCommentSymbol: str = symbolMapping["symbols"].get(extension)
-        multiLineCommentSymbolPair: str = symbolMapping["multilined"].get(extension)
-        if multiLineCommentSymbolPair:
-            multiLineCommentSymbolPair = multiLineCommentSymbolPair.split(" ")
+        singleline_symbol: Optional[str] = symbolMapping["symbols"].get(extension)
+        multiline_symbols: Optional[str] = symbolMapping["multilined"].get(extension)
+        multiline_symbol_pair: Optional[list[str]] = None
 
-        if not (singleLineCommentSymbol or multiLineCommentSymbolPair):        
-            print(f"No comment symbols found for extension .{extension}")
-            exit(500)
-
-        if not multiLineCommentSymbolPair:
-            return singleLineCommentSymbol.encode()
-        if not singleLineCommentSymbol:
-            return multiLineCommentSymbolPair[0].encode(), multiLineCommentSymbolPair[1].encode()
+        if not (singleline_symbol or multiline_symbols):
+            raise ValueError(f"No comment symbols found for extension .{extension}")
         
-        return singleLineCommentSymbol.encode(), (multiLineCommentSymbolPair[0].encode(), multiLineCommentSymbolPair[1].encode())
+        if multiline_symbols:
+            multiline_symbol_pair = multiline_symbols.split()
+            if not len(multiline_symbol_pair) == 2:
+                raise ValueError(f"Invalid syntax for multiline comments for extension: .{extension}")
+        
+        if not singleline_symbol:
+            assert multiline_symbol_pair
+            return multiline_symbol_pair[0].encode(), multiline_symbol_pair[1].encode()
+        if not multiline_symbol_pair:
+            return singleline_symbol.encode()
+        
+        return (singleline_symbol.encode(),
+                (multiline_symbol_pair[0].encode(), multiline_symbol_pair[1].encode()))
 
 def dumpOutputSTD(outputMapping: dict, fpath: os.PathLike) -> None:
     '''Dump output to a standard text/log file'''
