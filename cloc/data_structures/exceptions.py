96b129232275b3cebb84609dd419e9c18b244acb
Parth <1008parth@gmail.com>
2026-01-16 17:23:58 +0530

    Adds missing message param to ExitException

--- cloc/data_structures/exceptions.py
+++ cloc/data_structures/exceptions.py
@@ -4,7 +4,7 @@ class ExitException(Exception):
     __slots__ = ("message",)
     def __init__(self, message: str, *args: object) -> None:
         self.message = message
-        super().__init__(*args)
+        super().__init__(message, *args)
 
 class InvalidConfigurationException(ExitException):
     def __init__(self, message: str = "Invalid configuration", *args: object) -> None:
