ace464d275ab7bfa39b1f5bbe639f16fd0e865fd
Parth <1008parth@gmail.com>
2026-01-21 19:14:53 +0530

    Updates line parsing in C to expose function back to Python

--- cloc/parse_file.c
+++ cloc/parse_file.c
@@ -2,9 +2,7 @@
 #include <Python.h>
 #include <stdbool.h>
 
-static const int NEWLINE_INTEGER = 10;
-
-static PyObject *_parse_file(PyObject *self, PyObject *args){
+static PyObject *_parse_memoryview(PyObject *self, PyObject *args){
     Py_buffer mapped_buffer;
 
     const char *singleline_character,
@@ -26,9 +24,10 @@ static PyObject *_parse_file(PyObject *self, PyObject *args){
     }
 
     bool commented_block = false;
-    int total_lines = 0, loc = 0;
+    int total_lines = 0, loc = 0, valid_chars = 0;
 
     // Parsing logic here >:3
+    const char *view = (const char *) mapped_buffer.buf;
     for (Py_ssize_t i = 0; i < mapped_buffer.len; i++){
         
     }
@@ -36,4 +35,28 @@ static PyObject *_parse_file(PyObject *self, PyObject *args){
     PyBuffer_Release(&mapped_buffer);
 
     return Py_BuildValue("ii", total_lines, loc);
+}
+
+static PyMethodDef methods[] = {
+    {
+        .ml_name = "_parse_memoryview",
+        .ml_doc = "Parse a UTF-8 byte stream to count total lines and lines of code (LOC)",
+        .ml_flags = METH_VARARGS,
+        .ml_meth = _parse_memoryview
+    },
+    {NULL, NULL, 0, NULL}
+};
+
+PyDoc_STRVAR(module_doc, "Internal module for parsing files");
+static PyModuleDef module = {
+    .m_base = PyModuleDef_HEAD_INIT,
+    .m_name = "parse_file",
+    .m_doc = module_doc,
+    .m_size = -1,
+    .m_methods = methods
+};
+
+PyMODINIT_FUNC
+PyInit__parse_file(void){
+    return PyModule_Create(&module);
 }
