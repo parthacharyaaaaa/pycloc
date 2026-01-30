6dafb21716ca72c30adf6c232b3b1faa8260c4a4
Parth <1008parth@gmail.com>
2026-01-30 16:05:47 +0530

    [UPDATE] Updates data_structures' __init__ to include all structures

--- cloc/data_structures/__init__.py
+++ cloc/data_structures/__init__.py
@@ -1,5 +1,15 @@
 '''Data structures used within the pycloc package'''
 
-from .exceptions import (ExitException, InvalidConfigurationException)
+from cloc.data_structures.exceptions import (ExitException,
+                                             InvalidConfigurationException)
+from cloc.data_structures.singleton import SingletonMeta
+from cloc.data_structures.parse_modes import ParseMode
+from cloc.data_structures.config import ClocConfig
+import cloc.data_structures.typing as cloc_typing
 
-__all__ = ("ExitException", "InvalidConfigurationException")
+__all__ = ("ExitException",
+           "InvalidConfigurationException",
+           "SingletonMeta",
+           "ParseMode",
+           "ClocConfig",
+           "cloc_typing")
